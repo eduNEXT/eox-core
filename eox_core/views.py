ba29f43325598451451763043b1028f5b476cc57
Felipe Montoya <felipe.montoya@edunext.co>
2018-07-27 12:08:22 -0500

    Adding git sha to the output of eox-info

--- eox_core/views.py
+++ eox_core/views.py
@@ -3,8 +3,10 @@
 from __future__ import unicode_literals
 
 import json
+from subprocess import check_output
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 import eox_core
 
@@ -13,9 +15,15 @@ def default_view(request):
     """
     The HTTP endopoint to get the eox-core API version
     """
+    try:
+        git_data = unicode(check_output(["git", "rev-parse", "HEAD"]))
+    except Exception as e:
+        git_data = ""
+
     response_data = {
         "version": eox_core.__version__,
         "name": "eox-core",
+        "git": git_data.rstrip('\r\n'),
     }
     return HttpResponse(
         json.dumps(response_data),
