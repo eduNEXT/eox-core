661312b51d43954c3516f04ea228535e0eb5f51b
Ivan Castellanos <ivanca@gmail.com>
2018-08-01 10:12:34 -0500

    Finish coding "test_user_is_logged_in" on test_views.py

--- eox_core/tests/test_views.py
+++ eox_core/tests/test_views.py
@@ -3,8 +3,12 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.test import TestCase
-from ..test_utils import SuperUserFactory, DEFAULT_PASSWORD
+from ..test_utils import SuperUserFactory
 import eox_core
+from rest_framework.test import APIRequestFactory, force_authenticate
+from eox_core.api.v1.views import UserInfo
+
+JSON_CONTENT_TYPE = 'application/json'
 
 
 class TestInfoView(TestCase):
@@ -16,5 +20,20 @@ class TestInfoView(TestCase):
         self.assertContains(response, eox_core.__version__)
 
     def test_user_is_logged_in(self):
-        self.superuser = SuperUserFactory()
-        self.client.login(username=self.superuser.username, password=DEFAULT_PASSWORD)
+        factory = APIRequestFactory()
+        view = UserInfo.as_view()
+        self.user = SuperUserFactory()
+        request = factory.get('/api/v1/userinfo', content_type='application/json')
+        force_authenticate(request, user=self.user)
+        response = view(request)
+        response_json = response.data
+        self.assertIn('user', response_json)
+        self.assertDictEqual(
+            response_json, {
+                u'is_superuser': True,
+                u'is_staff': True,
+                u'user': u'robot0',
+                u'auth': u'None',
+                u'email': u'robot+test+0@edx.org'
+            }
+        )
