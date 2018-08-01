515edb88a7dd2f08186259fb41e48fcdd5f2ce49
Ivan Castellanos <ivanca@gmail.com>
2018-08-01 10:11:08 -0500

    cleaning eox-core/views.py

--- eox_core/views.py
+++ eox_core/views.py
@@ -6,15 +6,8 @@ from __future__ import unicode_literals
 import json
 from os.path import dirname, realpath
 from subprocess import check_output
-from rest_framework.views import APIView
 
 from django.http import HttpResponse
-from rest_framework_oauth.authentication import OAuth2Authentication
-from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import IsAdminUser
-from rest_framework.response import Response
-from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
-from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 
 import eox_core
@@ -28,7 +21,7 @@ def info_view(request):
     try:
         working_dir = dirname(realpath(__file__))
         git_data = unicode(check_output(["git", "rev-parse", "HEAD"], cwd=working_dir))
-    except Exception as e:
+    except Exception:
         git_data = ""
 
     response_data = {
