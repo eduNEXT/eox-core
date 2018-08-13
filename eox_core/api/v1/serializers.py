d6d3ac8f6f21281ef56dfe1eb7a5085a54519217
Ivan Castellanos <ivanca@gmail.com>
2018-08-13 15:28:28 -0500

    allow enrollments when using array (json)

--- eox_core/api/v1/serializers.py
+++ eox_core/api/v1/serializers.py
@@ -85,4 +85,4 @@ class EdxappCourseEnrollmentQuerySerializer(EdxappCourseEnrollmentSerializer):
     email = serializers.CharField(max_length=255, default=None)
     force_registration = serializers.BooleanField(default=False)
     course_id = serializers.CharField(max_length=255)
-    enrollment_attributes = EdxappEnrollmentAttributeSerializer(many=True)
+    enrollment_attributes = EdxappEnrollmentAttributeSerializer(many=True, default=[])
