46b10f874cb1915ffd602d23ecda6cf3776ce055
Ivan Castellanos <ivanca@gmail.com>
2018-08-28 15:55:31 -0500

    Allow invitation-only enrollments (AKA check_access=False) We had to copy the internals of the create_course_enrollment function to use check_access=False instead of check_access=True when calling CourseEnrollment.enroll

--- eox_core/edxapp_wrapper/backends/enrollment_h_v1.py
+++ eox_core/edxapp_wrapper/backends/enrollment_h_v1.py
@@ -17,7 +17,19 @@ from enrollment.errors import (
 from openedx.core.lib.exceptions import CourseNotFoundError
 from course_modes.models import CourseMode
 from rest_framework.exceptions import APIException
-from django.contrib.auth import get_user_model
+from opaque_keys.edx.keys import CourseKey
+from django.contrib.auth.models import User
+from six import text_type
+from student.models import (
+    AlreadyEnrolledError,
+    CourseEnrollment,
+    CourseFullError,
+    EnrollmentClosedError,
+    NonExistentCourseError
+)
+import logging
+
+log = logging.getLogger(__name__)
 
 
 def create_enrollment(*args, **kwargs):
@@ -34,13 +46,41 @@ def create_enrollment(*args, **kwargs):
     if validation_errors:
         return None, [", ".join(validation_errors)]
     if email:
-        match = get_user_model().objects.filter(email=email).first()
+        match = User.objects.filter(email=email).first()
         if match is None:
             raise APIException('No user found with that email')
         else:
             username = match.username
     try:
-        enrollment = api._data_api().create_course_enrollment(username, course_id, mode, is_active)
+
+        if force_registration:
+            # code copied from edx-platform/common/djangoapps/enrollment/data.py@create_course_enrollment
+            # the only change is check_access=True to check_access=False and remove "return" keyword
+            # (Open edx version=0.10)
+            course_key = CourseKey.from_string(course_id)
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                msg = u"Not user with username '{username}' found.".format(username=username)
+                log.warn(msg)
+                raise UserNotFoundError(msg)
+            try:
+                enrollment = CourseEnrollment.enroll(user, course_key, check_access=False)
+                api._data_api()._update_enrollment(enrollment, is_active=is_active, mode=mode)
+            except NonExistentCourseError as err:
+                raise CourseNotFoundError(text_type(err))
+            except EnrollmentClosedError as err:
+                raise CourseEnrollmentClosedError(text_type(err))
+            except CourseFullError as err:
+                raise CourseEnrollmentFullError(text_type(err))
+            except AlreadyEnrolledError as err:
+                enrollment = api._data_api().get_course_enrollment(username, course_id)
+                raise CourseEnrollmentExistsError(text_type(err), enrollment)
+            # code copy ends
+        else:
+            # if force registration is false we just call the open edx-platform function create_course_enrollment
+            enrollment = api._data_api().create_course_enrollment(username, course_id, mode, is_active)
+
         if enrollment_attributes is not None:
             api.set_enrollment_attributes(username, course_id, enrollment_attributes)
     except CourseEnrollmentExistsError as e:
@@ -50,6 +90,7 @@ def create_enrollment(*args, **kwargs):
             raise APIException(repr(e) + ", use force_registration to update the existing enrollment")
     except (UserNotFoundError, InvalidEnrollmentAttribute, CourseNotFoundError, CourseEnrollmentFullError, CourseEnrollmentClosedError) as e:
         raise APIException(repr(e))
+
     return enrollment, errors
 
 
