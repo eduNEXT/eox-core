56d530e8cf09c4cf7be6ba095c1610261c06ef1c
Felipe Montoya <felipe.montoya@edunext.co>
2018-08-28 18:57:49 -0500

    fixing pylint issues

--- eox_core/edxapp_wrapper/backends/enrollment_h_v1.py
+++ eox_core/edxapp_wrapper/backends/enrollment_h_v1.py
@@ -3,23 +3,25 @@
 """
 Backend for the create_edxapp_user that works under the open-release/hawthorn.beta1 tag
 """
-
+# pylint: disable=import-error, protected-access
 from __future__ import absolute_import, unicode_literals
+import logging
+from django.contrib.auth.models import User
 from enrollment import api
 from enrollment.errors import CourseModeNotFoundError
 from enrollment.errors import CourseEnrollmentExistsError
 from course_modes.models import CourseMode
 from rest_framework.exceptions import APIException
 from opaque_keys.edx.keys import CourseKey
-from django.contrib.auth.models import User
 from student.models import CourseEnrollment
-import logging
 
-log = logging.getLogger(__name__)
+LOG = logging.getLogger(__name__)
 
 
 def create_enrollment(*args, **kwargs):
-
+    """
+    backend function to create enrollment
+    """
     errors = []
     email = kwargs.get("email")
     username = kwargs.get('username')
@@ -40,11 +42,11 @@ def create_enrollment(*args, **kwargs):
 
     try:
         enrollment = _create_or_update_enrollment(username, course_id, mode, is_active, force_registration)
-    except Exception as e:
+    except Exception as err:  # pylint: disable=broad-except
         if force_registration:
             enrollment = _force_create_enrollment(username, course_id, mode, is_active)
         else:
-            raise APIException(repr(e))
+            raise APIException(repr(err))
 
     if enrollment_attributes is not None:
         api.set_enrollment_attributes(username, course_id, enrollment_attributes)
@@ -52,7 +54,11 @@ def create_enrollment(*args, **kwargs):
     return enrollment, errors
 
 
+# pylint: disable=invalid-name
 def check_edxapp_enrollment_is_valid(*args, **kwargs):
+    """
+    backend function to check if enrollment is valid
+    """
     errors = []
     is_active = kwargs.get("is_active", True)
     course_id = kwargs.get("course_id")
@@ -73,24 +79,30 @@ def check_edxapp_enrollment_is_valid(*args, **kwargs):
 
 
 def _create_or_update_enrollment(username, course_id, mode, is_active, try_update):
+    """
+    non-forced create or update enrollment internal function
+    """
     try:
         enrollment = api._data_api().create_course_enrollment(username, course_id, mode, is_active)
-    except CourseEnrollmentExistsError as e:
+    except CourseEnrollmentExistsError as err:
         if try_update:
             enrollment = api._data_api().update_course_enrollment(username, course_id, mode, is_active)
         else:
-            raise Exception(repr(e) + ", use force_registration to update the existing enrollment")
+            raise Exception(repr(err) + ", use force_registration to update the existing enrollment")
     return enrollment
 
 
 def _force_create_enrollment(username, course_id, mode, is_active):
-    log.info('Calling _force_create_enrollment')
+    """
+    forced create enrollment internal function
+    """
+    LOG.info('Calling _force_create_enrollment')
     try:
         course_key = CourseKey.from_string(course_id)
         user = User.objects.get(username=username)
         enrollment = CourseEnrollment.enroll(user, course_key, check_access=False)
         api._data_api()._update_enrollment(enrollment, is_active=is_active, mode=mode)
-    except Exception as e:
-        log.warn('API call failed: {}, {}, {}, {}, {}'.format(repr(e), username, course_id, mode, is_active))
-        raise APIException(repr(e))
+    except Exception as err:  # pylint: disable=broad-except
+        LOG.warn('API call failed: %s, %s, %s, %s, %s', repr(err), username, course_id, mode, is_active)
+        raise APIException(repr(err))
     return enrollment
