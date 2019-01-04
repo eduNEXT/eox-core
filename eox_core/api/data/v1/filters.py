2772081119a31e6bd947771e6e739730c154f769
Squirrel18 <daniel.quiroga@edunext.co>
2019-01-04 17:11:20 -0500

    Remove unused method filters.

--- eox_core/api/data/v1/filters.py
+++ eox_core/api/data/v1/filters.py
@@ -128,8 +128,8 @@ class GeneratedCerticatesFilter(BaseDataApiFilter):
     created_date = django_filters.DateTimeFromToRangeFilter()
     course_id = django_filters.CharFilter(method="filter_course_id")
     status = django_filters.CharFilter(method="filter_status")
-    grade = django_filters.CharFilter(method="filter_status")
-    mode = django_filters.CharFilter(method="filter_status")
+    grade = django_filters.CharFilter()
+    mode = django_filters.CharFilter()
 
     def filter_course_id(self, queryset, value):
         """
