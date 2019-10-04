a131c05f640fc3b7b312d9ad7af1d245aa46b536
jfavellar90 <jhony.avella@edunext.co>
2019-10-03 20:16:22 -0500

    Defining all methods on the users test backend based on the original production users backend

--- eox_core/edxapp_wrapper/backends/users_h_v1_test.py
+++ eox_core/edxapp_wrapper/backends/users_h_v1_test.py
@@ -5,6 +5,46 @@ Test backend to get CourseEnrollment Model.
 from django.contrib.auth.models import Permission
 
 
+def get_edxapp_user(**kwargs):
+    """
+    Return a fake user
+    """
+    return object
+
+
+def create_edxapp_user(*args, **kwargs):
+    """
+    Return a fake user and a list of errors
+    """
+    return object, []
+
+
+def get_user_read_only_serializer():
+    """
+    Return a fake user read only serializer
+    """
+    try:
+        from openedx.core.djangoapps.user_api.accounts.serializers import UserReadOnlySerializer
+    except ImportError:
+        UserReadOnlySerializer = object
+    return UserReadOnlySerializer
+
+
+def check_edxapp_account_conflicts(email, username):
+    """
+    Get an executes the check_account_exists for tests
+    """
+    try:
+        from openedx.core.djangoapps.user_api.accounts.api import check_account_exists
+    except ImportError:
+        def check_account_exists(email=None, username=None):
+            """
+            Fake definition for check_account_exists edxapp function
+            """
+            return email and username
+    return check_account_exists(email=email, username=username)
+
+
 def get_course_enrollment():
     """
     Get Test CourseEnrollment model.
@@ -13,3 +53,32 @@ def get_course_enrollment():
     django-filters is happy and no migrations are created.
     """
     return Permission
+
+
+def get_course_team_user(*args, **kwargs):
+    """
+    Return a fake course team user
+    """
+    return object
+
+
+def get_user_signup_source():
+    """
+    Get test UserSignupSource model
+    """
+    try:
+        from student.models import UserSignupSource
+    except ImportError:
+        UserSignupSource = object
+    return UserSignupSource
+
+
+def get_login_failures():
+    """
+    Get test LoginFailures model
+    """
+    try:
+        from student.models import LoginFailures
+    except ImportError:
+        LoginFailures = object
+    return LoginFailures
