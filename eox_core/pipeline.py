aca46a5818b636420f0bb9cebec77b0b79c175a1
Andrey Canon <andrey.canon@edunext.co>
2020-02-07 16:18:59 -0500

    Force the execution of user_post_save_callback in order to execute the auto enrollments.

--- eox_core/pipeline.py
+++ eox_core/pipeline.py
@@ -2,6 +2,9 @@
 The pipeline module defines functions that are used in the third party authentication flow
 """
 import logging
+
+from django.db.models.signals import post_save
+
 from eox_core.edxapp_wrapper.users import get_user_profile
 
 LOG = logging.getLogger(__name__)
@@ -20,3 +23,44 @@ def ensure_user_has_profile(backend, details, user=None, *args, **kwargs):
         except user_profile_model.DoesNotExist:
             user_profile_model.objects.create(user=user)
             LOG.info('Created new profile for user during the third party pipeline: "%s"', user)
+
+
+def force_user_post_save_callback(auth_entry, is_new, user=None, *args, **kwargs):
+    """
+    Send the signal post_save in order to force the execution of user_post_save_callback, see it in
+    https://github.com/eduNEXT/edunext-platform/blob/4169327231de00991c46b6192327fe50b0406561/common/djangoapps/student/models.py#L652
+    this allows the automatic enrollments if a user is registered by a third party auth.
+    This depends on "third_party_auth.pipeline.parse_query_params".
+
+    the discussion left three options:
+
+    1)
+        user._changed_fields = {'is_active': user.is_active}  # pylint: disable=protected-access
+        post_save.send_robust(
+            sender=user.__class__,
+            instance=user,
+            created=False
+        )
+
+    2)
+        user.is_active = False
+        user.save()
+        user.is_active = True
+        user.save()
+
+    3)
+        registration = Registration.objects.get_or_create(user=user)
+        user.is_active = False
+        user.save()
+        registration.activate()
+
+    The first one was selected because the second executed multiple unnecessary process and third one
+    needs to implement a new backend.
+    """
+    if auth_entry == 'register' and user and is_new:
+        user._changed_fields = {'is_active': user.is_active}  # pylint: disable=protected-access
+        post_save.send_robust(
+            sender=user.__class__,
+            instance=user,
+            created=False
+        )
