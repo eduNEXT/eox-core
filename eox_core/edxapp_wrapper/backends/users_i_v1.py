994a119a31b997f6a431c3df497e40b74caa1536
Luis Moreno <luis.moreno@edunext.co>
2020-04-15 13:17:11 -0400

    Removed ironwood user backend since it only imports the contents of hawthorn backend.

--- eox_core/edxapp_wrapper/backends/users_i_v1.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Backend for the create_edxapp_user that works under the open-release/ironwood.master tag
-"""
-from __future__ import absolute_import, unicode_literals
-from eox_core.edxapp_wrapper.backends.users_h_v1 import *  # pylint: disable=wildcard-import,unused-wildcard-import
