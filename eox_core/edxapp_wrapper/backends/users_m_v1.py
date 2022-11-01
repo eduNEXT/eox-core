d2f4e557883b7cbb1a5ca9d4b7432204ca1b235e
magajh <maria.jaimes@edunext.co>
2022-11-01 12:17:05 -0400

    test

--- eox_core/edxapp_wrapper/backends/users_m_v1.py
+++ eox_core/edxapp_wrapper/backends/users_m_v1.py
@@ -113,6 +113,7 @@ class EdnxAccountCreationForm(AccountCreationForm):
     This form it's currently only used for validation, not rendering.
     """
 
+
     def __init__(  # pylint:disable=too-many-arguments
             self,
             data=None,
