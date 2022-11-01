9da6d83f9d60b94be078a4caf8d8a09f7cc940d1
magajh <maria.jaimes@edunext.co>
2022-11-01 19:43:39 -0400

    feat: add endpoint to create edxapp Oauth Application

--- eox_core/api/support/v1/urls.py
+++ eox_core/api/support/v1/urls.py
@@ -9,4 +9,5 @@ app_name = 'eox_core'  # pylint: disable=invalid-name
 urlpatterns = [  # pylint: disable=invalid-name
     url(r'^user/$', views.EdxappUser.as_view(), name='edxapp-user'),
     url(r'^user/replace-username/$', views.EdxappReplaceUsername.as_view(), name='edxapp-replace-username'),
+    url(r'^oauth-application/$', views.OauthApplicationAPIView.as_view(), name='edxapp-oauth-application'),
 ]
