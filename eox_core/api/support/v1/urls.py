bb15fccfa437b7e537d9eae7a725794d1945b659
María Fernanda Magallanes <35668326+MaferMazu@users.noreply.github.com>
2024-01-25 11:57:56 -0500

    fix: update urls in favor of re_path for deprecation (#260)

--- eox_core/api/support/v1/urls.py
+++ eox_core/api/support/v1/urls.py
@@ -1,13 +1,13 @@
 """ urls.py """
 
-from django.conf.urls import url
+from django.urls import re_path
 
 from eox_core.api.support.v1 import views
 
 app_name = 'eox_core'  # pylint: disable=invalid-name
 
 urlpatterns = [  # pylint: disable=invalid-name
-    url(r'^user/$', views.EdxappUser.as_view(), name='edxapp-user'),
-    url(r'^user/replace-username/$', views.EdxappReplaceUsername.as_view(), name='edxapp-replace-username'),
-    url(r'^oauth-application/$', views.OauthApplicationAPIView.as_view(), name='edxapp-oauth-application'),
+    re_path(r'^user/$', views.EdxappUser.as_view(), name='edxapp-user'),
+    re_path(r'^user/replace-username/$', views.EdxappReplaceUsername.as_view(), name='edxapp-replace-username'),
+    re_path(r'^oauth-application/$', views.OauthApplicationAPIView.as_view(), name='edxapp-oauth-application'),
 ]
