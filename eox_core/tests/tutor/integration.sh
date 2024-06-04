94212eb53665e3ae5819f69c0584113309e165e8
Luis Felipe Castano <felipe.castano@edunext.co>
2024-06-04 01:44:18 -0500

    fix: quality changes

--- eox_core/tests/tutor/integration.sh
+++ eox_core/tests/tutor/integration.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# Install the package in the edxapp env
+# This script installs the package in the edxapp environment, installs test requirements from Open edX and runs the tests using the Tutor settings.
 echo "Install package"
 pip install -e ../eox-core
 
-# Install test requirements from openedx
 echo "Install test-requirements"
 make test-requirements
 
-# Running the tests using the tutor settings
 echo "Run tests"
 pytest -s --ds=lms.envs.tutor.test /openedx/eox-core/eox_core/tests/tutor
