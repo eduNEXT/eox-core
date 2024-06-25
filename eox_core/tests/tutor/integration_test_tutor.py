1176b68864847c2d3eec412e69e4c718106c161f
Luis Felipe Castaño <78836902+luisfelipec95@users.noreply.github.com>
2024-06-25 16:02:35 -0500

    feat: add api integration test (#272)
    * feat: add api integration test
    
    * fix: quality changes

--- eox_core/tests/tutor/integration_test_tutor.py
+++ eox_core/tests/tutor/integration_test_tutor.py
@@ -1,13 +1,20 @@
 """
 Test integration file.
 """
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
 
+@override_settings(ALLOWED_HOSTS=['testserver'], SITE_ID=2)
 class TutorIntegrationTestCase(TestCase):
     """
     Tests integration with openedx
     """
+
+    def setUp(self):
+        """
+        Set up the base URL for the tests
+        """
+        self.base_url = 'http://local.edly.io'
 
     # pylint: disable=import-outside-toplevel,unused-import
     def test_current_settings_code_imports(self):
@@ -24,3 +31,18 @@ class TutorIntegrationTestCase(TestCase):
         import eox_core.edxapp_wrapper.backends.pre_enrollment_l_v1  # isort:skip
         import eox_core.edxapp_wrapper.backends.storages_i_v1  # isort:skip
         import eox_core.edxapp_wrapper.backends.third_party_auth_l_v1  # isort:skip
+
+    def test_info_view(self):
+        """
+        Tests the info view endpoint in Tutor
+        """
+        info_view_url = f'{self.base_url}/eox-core/eox-info'
+
+        response = self.client.get(info_view_url)
+
+        self.assertEqual(response.status_code, 200)
+
+        response_data = response.json()
+        self.assertIn('version', response_data)
+        self.assertIn('name', response_data)
+        self.assertIn('git', response_data)
