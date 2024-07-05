678b9debb6ed041f395b4fed64e64a08312b295f
Bryann Valderrama <bryanttandrei2015@gmail.com>
2024-07-05 16:31:11 -0500

    fix: add basename in routers to avoid conflicts

--- eox_core/api/data/v1/routers.py
+++ eox_core/api/data/v1/routers.py
@@ -1,6 +1,7 @@
 """
-TODO: add me
+Routers for the API views.
 """
+
 from rest_framework import routers
 
 from .viewsets import (
@@ -12,9 +13,11 @@ from .viewsets import (
 )
 
 ROUTER = routers.DefaultRouter()
-ROUTER.register(r'users', UsersViewSet)
-ROUTER.register(r'course-enrollments', CourseEnrollmentViewset)
-ROUTER.register(r'certificates', CertificateViewSet, basename='generated_certificate')
-ROUTER.register(r'proctored-exams-attempts', ProctoredExamStudentViewSet)
+ROUTER.register(r"users", UsersViewSet, basename="users")
+ROUTER.register(r"course-enrollments", CourseEnrollmentViewset, basename="course-enrollments")
+ROUTER.register(r"certificates", CertificateViewSet, basename="generated_certificate")
+ROUTER.register(r"proctored-exams-attempts", ProctoredExamStudentViewSet, basename="proctored_exams_attempts")
 # Async operations
-ROUTER.register(r'async/course-enrollments-grades', CourseEnrollmentWithGradesViewset)
+ROUTER.register(
+    r"async/course-enrollments-grades", CourseEnrollmentWithGradesViewset, basename="async_course-enrollments-grades"
+)
