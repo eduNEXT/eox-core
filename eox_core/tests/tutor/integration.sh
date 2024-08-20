ca87511e95fc2fde44c298cd9a1c5a2195e1e7e6
Bryann Valderrama <bryann.valderrama@edunext.co>
2024-08-20 17:35:00 -0500

    chore: install eox-tenant after test-requirements and add colors

--- eox_core/tests/tutor/integration.sh
+++ eox_core/tests/tutor/integration.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+bgreen () { printf "\n\n\e[1m\e[32m" ; $@ ; printf "\e[0m\n\n"; }
+
 # This script installs the package in the edxapp environment, installs test
 # requirements from Open edX and runs the tests using the Tutor settings.
-echo "Install package"
+bgreen echo "Install package"
 pip install -e /openedx/eox-core
 
-echo "Install eox-tenant(requirement)"
-pip install eox-tenant
-
-echo "Run migrations"
-python manage.py lms makemigrations
-python manage.py lms migrate
-
-echo "Install test-requirements"
+bgreen echo "Install test-requirements"
 make test-requirements
 
-echo "Run tests"
+bgreen echo "Install eox-tenant(requirement)"
+pip install eox-tenant
+
+# echo "Run migrations"
+# python manage.py lms makemigrations
+# python manage.py lms migrate
+
+bgreen echo "Run tests"
 pytest -s --ds=lms.envs.tutor.test /openedx/eox-core/eox_core/tests/tutor
