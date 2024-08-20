9bf58cbf7ef5e18826c848b9e8ed0e2a0ddc3921
Bryann Valderrama <bryann.valderrama@edunext.co>
2024-08-20 17:49:42 -0500

    chore: remove colors in prints

--- eox_core/tests/tutor/integration.sh
+++ eox_core/tests/tutor/integration.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-bgreen () { printf "\n\n\e[1m\e[32m" ; $@ ; printf "\e[0m\n\n"; }
+# bgreen () { printf "\n\n\e[1m\e[32m" ; $@ ; printf "\e[0m\n\n"; }
 
 # This script installs the package in the edxapp environment, installs test
 # requirements from Open edX and runs the tests using the Tutor settings.
-bgreen echo "Install package"
+echo "Install package"
 pip install -e /openedx/eox-core
 
-bgreen echo "Install test-requirements"
+echo "Install test-requirements"
 make test-requirements
 
-bgreen echo "Install eox-tenant(requirement)"
+echo "Install eox-tenant(requirement)"
 pip install eox-tenant
 
 # echo "Run migrations"
 # python manage.py lms makemigrations
 # python manage.py lms migrate
 
-bgreen echo "Run tests"
+echo "Run tests"
 pytest -s --ds=lms.envs.tutor.test /openedx/eox-core/eox_core/tests/tutor
