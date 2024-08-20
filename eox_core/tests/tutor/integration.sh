9e652c4f1e518a955e5a669910b05806bad27043
Bryann Valderrama <bryann.valderrama@edunext.co>
2024-08-20 18:00:45 -0500

    chore: run migrations after eox-tenant installation

--- eox_core/tests/tutor/integration.sh
+++ eox_core/tests/tutor/integration.sh
@@ -13,9 +13,9 @@ make test-requirements
 bgreen echo "Install eox-tenant(requirement)"
 pip install eox-tenant
 
-# bgreen echo "Run migrations"
-# python manage.py lms makemigrations
-# python manage.py lms migrate
+bgreen echo "Run migrations"
+python manage.py lms makemigrations
+python manage.py lms migrate
 
 bgreen echo "Run tests"
 pytest -s --ds=lms.envs.tutor.test /openedx/eox-core/eox_core/tests/tutor
