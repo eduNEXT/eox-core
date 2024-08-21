4e8a2ba17fcf1897d2b01071dc67f6ec2cf5a4c0
Bryann Valderrama <bryann.valderrama@edunext.co>
2024-08-20 19:16:18 -0500

    chore: run migrations before install eox-core package

--- eox_core/tests/tutor/integration.sh
+++ eox_core/tests/tutor/integration.sh
@@ -7,13 +7,12 @@ bgreen () { printf "\n\n\e[1m\e[32m" ; $@ ; printf "\e[0m\n\n"; }
 bgreen echo "Install package"
 pip install -e /openedx/eox-core
 
-bgreen echo "Install test-requirements"
-make test-requirements
-
-bgreen echo "Install eox-tenant (requirement)"
-pip install eox-tenant
+bgreen echo "Run migrations"
 python manage.py lms makemigrations
 python manage.py lms migrate
+
+bgreen echo "Install test-requirements"
+make test-requirements
 
 bgreen echo "Run tests"
 pytest -s --ds=lms.envs.tutor.test /openedx/eox-core/eox_core/tests/tutor
