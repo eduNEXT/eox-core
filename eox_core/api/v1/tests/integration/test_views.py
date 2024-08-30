7f7694dca3f1d2b856bddd95d01281ee6d27ef74
Bryann Valderrama <bryann.valderrama@edunext.co>
2024-08-30 09:06:25 -0500

    chore: add fixtures to test base class

--- eox_core/api/v1/tests/integration/test_views.py
+++ eox_core/api/v1/tests/integration/test_views.py
@@ -3,64 +3,39 @@ Integration test suite for the API v1 views.
 """
 
 from django.contrib.auth.models import User
-from django.contrib.sites.models import Site
 from django.http import HttpResponse
-from django.test import TestCase, override_settings
+from django.test import override_settings
 from django.urls import reverse
-
-try:
-    from eox_tenant.models import Route, TenantConfig
-except ImportError:
-    Route = None
-    TenantConfig = None
-from oauth2_provider.models import Application
 from rest_framework import status
+from rest_framework.test import APITestCase
 
-CLIENT_ID = "apiclient"
-CLIENT_SECRET = "apisecret"
 EOX_CORE_USERS_BACKEND = "eox_core.edxapp_wrapper.backends.users_m_v1"
 
 
-def create_oauth2_client(user: User) -> None:
-    """
-    Create a new OAuth2 client.
-
-    Args:
-        user (User): The user that will own the client.
-    """
-    Application.objects.create(
-        name="eox-core-app",
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        client_type=Application.CLIENT_CONFIDENTIAL,
-        authorization_grant_type=Application.GRANT_CLIENT_CREDENTIALS,
-        user=user,
-    )
-
-
-def create_admin_user() -> User:
-    """
-    Create a new admin user.
-
-    Returns:
-        User: The admin user.
-    """
-    return User.objects.create_superuser("eox-core-admin", "eox-core@mail.com", "p@$$w0rd")
-
-
 @override_settings(ALLOWED_HOSTS=["*"], SITE_ID=2)
-class TestUsersAPIIntegration(TestCase):
-    """Integration test suite for the Users API"""
+class BaseAPIIntegrationTest(APITestCase):
+    """
+    Base class for the integration test suite.
+    """
 
     def setUp(self):
-        """Set up the test suite"""
-        self.path = reverse("eox-core:eox-api:eox-api:edxapp-user")
-        self.admin_user = create_admin_user()
-        create_oauth2_client(self.admin_user)
+        """
+        Set up the test suite.
+        """
+        self.admin_user = self.create_admin_user()
+        self.client.force_authenticate(user=self.admin_user)
         self.tenant_x = self.create_tenant("tenant-x")
         self.tenant_y = self.create_tenant("tenant-y")
 
-    @override_settings(EOX_CORE_USERS_BACKEND=EOX_CORE_USERS_BACKEND)
+    def create_admin_user(self) -> User:
+        """
+        Create a new admin user.
+
+        Returns:
+            User: The admin user.
+        """
+        return User.objects.create_superuser("eox-core-admin", "eox-core@mail.com", "p@$$w0rd")
+
     def create_tenant(self, tenant: str) -> dict:
         """
         Create a new tenant.
@@ -71,40 +46,16 @@ class TestUsersAPIIntegration(TestCase):
         Returns:
             dict: The tenant data.
         """
-        domain = f"{tenant}.local.edly.io"
-        name = tenant.capitalize()
-        config = TenantConfig.objects.create(
-            external_key=f"{domain}-key",
-            lms_configs={
-                "EDNX_USE_SIGNAL": True,
-                "PLATFORM_NAME": name,
-                "SITE_NAME": domain,
-                "course_org_filter": ["OpenedX"],
-            },
-        )
-        Route.objects.create(domain=domain, config=config)  # pylint: disable=no-member
-        Site.objects.create(domain=domain, name=name)
-        return {"domain": domain, "token": self.get_access_token(domain)}
+        return {"domain": f"{tenant}.local.edly.io"}
 
-    def get_access_token(self, tenant_domain: str) -> str:
-        """
-        Get an access token for a tenant.
 
-        Args:
-            tenant_domain (str): The tenant domain.
+class TestUsersAPIIntegration(BaseAPIIntegrationTest):
+    """Integration test suite for the Users API"""
 
-        Returns:
-            str: The access token.
-        """
-        data = {
-            "client_id": CLIENT_ID,
-            "client_secret": CLIENT_SECRET,
-            "grant_type": "client_credentials",
-        }
-        headers = {"Host": tenant_domain}
-        path = f"http://{tenant_domain}/oauth2/access_token/"
-        response = self.client.post(path, data=data, headers=headers)
-        return response.json()["access_token"]
+    def setUp(self):
+        """Set up the test suite"""
+        self.path = reverse("eox-core:eox-api:eox-api:edxapp-user")
+        super().setUp()
 
     def create_user_in_tenant(self, tenant: dict, user_data: dict) -> HttpResponse:
         """
@@ -117,7 +68,7 @@ class TestUsersAPIIntegration(TestCase):
         Returns:
             HttpResponse: The response object.
         """
-        headers = {"Authorization": f"Bearer {tenant['token']}", "Host": tenant["domain"]}
+        headers = {"Host": tenant["domain"]}
         response = self.client.post(self.path, data=user_data, headers=headers)
         return response
 
@@ -175,21 +126,24 @@ class TestUsersAPIIntegration(TestCase):
         self.assertIn("username", response_data)
 
 
-@override_settings(ALLOWED_HOSTS=["testserver"], SITE_ID=2)
-class TestInfoView(TestCase):
+class TestInfoView(BaseAPIIntegrationTest):
     """
     Integration test suite for the info view.
     """
 
     def setUp(self):
         """
-        Set up the base URL for the tests
+        Set up the test suite.
         """
         self.path = reverse("eox-core:eox-info")
+        super().setUp()
 
-    def test_info_view(self):
-        """
-        Tests the info view endpoint in Tutor
+    def test_info_view_success(self):
+        """Test the info view.
+
+        Expected result:
+        - The status code is 200.
+        - The response contains the version, name and git commit hash.
         """
         response = self.client.get(self.path)
         response_data = response.json()
