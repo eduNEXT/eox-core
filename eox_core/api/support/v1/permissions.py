0cdbd4e102cf452e563e25373b0bb10dc15bcc1e
Maga Giorgianni <maria.jaimes@edunext.co>
2025-03-07 16:13:23 -0400

    feat: add JWT signed authentication for OAuth application API (#318)
    * feat(auth): add JWT signed authentication for OAuth application API
    
    * feat(support-api): allow AnonymousUser access only via JWT-signed OAuth authentication

--- eox_core/api/support/v1/permissions.py
+++ eox_core/api/support/v1/permissions.py
@@ -3,7 +3,10 @@
 """
 Custom Support API permissions module
 """
+from django.contrib.auth.models import AnonymousUser
 from rest_framework import exceptions, permissions
+
+from eox_core.api.support.v1.authentication import JWTsignedOauthAppAuthentication
 
 
 class EoxCoreSupportAPIPermission(permissions.BasePermission):
@@ -14,9 +17,15 @@ class EoxCoreSupportAPIPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
         """
-        For now, to grant access only checks if the requesting user:
-            1) is_staff
+        Grants access if:
+        1) The user was authenticated via the signed JWT token (AnonymousUser but authenticated).
+        2) The user is authenticated and is a staff user.
         """
+        if isinstance(request.user, AnonymousUser):
+            auth_class_used = getattr(request.successful_authenticator, "__class__", None)
+            if auth_class_used == JWTsignedOauthAppAuthentication:
+                return True
+
         if request.user.is_staff:
             return True
 
