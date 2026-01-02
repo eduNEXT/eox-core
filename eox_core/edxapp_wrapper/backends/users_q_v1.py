6177feebaa5f04b41ff712534af2f5bebea2a806
Maria Grimaldi <maria.grimaldi@edunext.co>
2026-01-02 13:15:07 +0100

    test: add test suite for permanently delete a user

--- eox_core/edxapp_wrapper/backends/users_q_v1.py
+++ eox_core/edxapp_wrapper/backends/users_q_v1.py
@@ -276,13 +276,13 @@ def permanently_delete_user(*args, **kwargs):
     user = kwargs.get("user")
 
     with transaction.atomic():
-        # Delete OAuth tokens associated with the user.
+        # Delete OAuth tokens associated with the user
+        # and unlink LMS social auth accounts
         retire_dot_oauth2_models(user)
-
-        # Unlink LMS social auth accounts
         UserSocialAuth.objects.filter(user_id=user.id).delete()
 
-        # Delete the user
+        # Permanently delete the user and rely on cascade deletion to remove
+        # its related objects
         user.delete()
 
     return (
